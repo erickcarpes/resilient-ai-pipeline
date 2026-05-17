5ede8b4389850165078dc6aa8f7a7beae0f0a2dd
Erick Carpes <erickcarpesmmc@gmail.com>
2026-05-17 19:20:18 -0300

    chore: add /dist to .gitignore

--- dist/apps/gateway/libs/shared/src/redis/redis.constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REDIS_CLIENT = void 0;
-exports.REDIS_CLIENT = Symbol('REDIS_CLIENT');
-//# sourceMappingURL=redis.constants.js.map
