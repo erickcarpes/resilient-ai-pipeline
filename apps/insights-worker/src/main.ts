261b7f94d2b83b7c67ac78ae3079933a3759f00f
Erick Carpes <erickcarpesmmc@gmail.com>
2026-05-19 13:52:48 -0300

    fix: deleted files

--- apps/insights-worker/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { InsightsWorkerModule } from './insights-worker.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(InsightsWorkerModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
--- apps/transcription-worker/src/transcription-worker.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class TranscriptionWorkerService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
